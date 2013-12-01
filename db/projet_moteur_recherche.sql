ca9cad6f61dd9b47cbb622eccb37a6302393aa5b
GIZOU <GIZOU@10.75.0.231>
2013-12-01 19:48:00 +0100

    Misa à jour de la BDD: table user pour la partie administration

--- db/projet_moteur_recherche.sql
+++ db/projet_moteur_recherche.sql
@@ -199,6 +199,26 @@ INSERT INTO `media_type` (`id_type`, `name`) VALUES
 (2, 'video'),
 (3, 'picture');
 
+
+
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
