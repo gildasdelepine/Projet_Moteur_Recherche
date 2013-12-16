b86e26e7afdff647e8d94d436a46ba11ecaa2dd0
Marc <Marc@Ocean>
2013-12-16 10:32:10 +0100

    Unzip ok

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -1,40 +1,45 @@
 <?php
 
 class Upload extends CI_Controller {
-	
-	public function __construct()
-	{
-		parent::__construct();
-		$this->load->helper(array('form', 'url'));
-	}
-	
-	public function index()
-	{	
-		$this->load->view('upload_form', array('error' => ' ' ));
-	}
 
-	public function do_upload()
-	{
-		$config['upload_path'] = './uploads/';
-		$config['allowed_types'] = 'gif|jpg|png';
-		$config['max_size']	= '1000000';
-		$config['max_width']  = '1920';
-		$config['max_height']  = '1080';
-		
-		$this->load->library('upload', $config);
-	
-		if ( ! $this->upload->do_upload())
-		{
-			$error = array('error' => $this->upload->display_errors());
-			
-			$this->load->view('upload_form', $error);
-		}	
-		else
-		{
-			$data = array('upload_data' => $this->upload->data());
-			
-			$this->load->view('upload_success', $data);
-		}
-	}	
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper(array('form', 'url'));
+    }
+
+    public function index() {
+        $this->load->view('upload_form', array('error' => ' '));
+    }
+
+    public function do_upload() {
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png|zip';
+        $config['max_size'] = '1000000';
+        $config['max_width'] = '1920';
+        $config['max_height'] = '1080';
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload()) {
+            $error = array('error' => $this->upload->display_errors());
+
+            $this->load->view('upload_form', $error);
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+            $upload_data = $data['upload_data'];
+        // Dezip le fichier mais mettre condition si l'upload est une image ou non
+        //    $this->extract_file($upload_data['file_name']);
+
+            $this->load->view('upload_success', $data);
+        }
+    }
+
+    public function extract_file($file_name) {
+        $this->load->library('unzip');
+        $this->unzip->allow(array('css', 'js', 'png', 'gif', 'jpeg', 'jpg', 'tpl', 'html', 'swf'));
+        $this->unzip->extract('./uploads/' . $file_name);
+    }
+
 }
+
 ?>
