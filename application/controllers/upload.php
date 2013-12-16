a7869dc1e7bf9dbe3017df1b42b8c95da4aeca58
Marc <Marc@Ocean>
2013-12-16 13:46:43 +0100

    repertoire upload modifié -> images/zip to do --> mettre img dans bdd cgi à faire pour: calculer signature calculer distance

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -12,7 +12,7 @@ class Upload extends CI_Controller {
     }
 
     public function do_upload() {
-        $config['upload_path'] = './uploads/zip';
+        $config['upload_path'] = './images/zip';
         $config['allowed_types'] = 'zip';
         $config['max_size'] = '1000000';
         $config['max_width'] = '1920';
@@ -21,7 +21,7 @@ class Upload extends CI_Controller {
         $this->load->library('upload', $config);
 
         if (!$this->upload->do_upload()) {
-            $error = array('error' => 'Ce fichier n est pas un zip !'/*$this->upload->display_errors()*/);
+            $error = array('error' => 'Ce fichier n est pas un zip !'/* $this->upload->display_errors() */);
             $this->load->view('upload_form', $error);
         } else {
             $data = array('upload_data' => $this->upload->data());
@@ -36,13 +36,22 @@ class Upload extends CI_Controller {
     public function extract_file($file_name, $raw_name) {
         $this->load->library('unzip');
         $this->unzip->allow(array('css', 'js', 'png', 'gif', 'jpeg', 'jpg', 'tpl', 'html', 'swf'));
-        $this->unzip->extract('./uploads/zip/' . $file_name, './uploads/' . $raw_name);
-        //unlink('./uploads/zip/' . $file_name);
-        // $this->descriptor_processing();
+        $this->unzip->extract('./images/zip/' . $file_name, './images/' . $raw_name);
+        //unlink('./images/zip/' . $file_name);
+        $this->meta_processing($raw_name);
     }
-    
-    public function descriptor_processing(){
+
+    public function meta_processing($raw_name) {
         // TO DO
+        $imgPath = './images/' . $raw_name;
+        $images = glob($imgPath, GLOB_BRACE);
+
+        foreach ($images as &$imgName) {
+            $fileName = $raw_name . '/' . $imgName;
+            $keywords = split("+", $imgName);
+            // TO DO --> Sending to DB.
+        }
+        unset($imgName);
     }
 
 }
